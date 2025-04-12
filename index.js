9224ef9d753c038b2f92b795cec52e774d499fe7
magi <magali.gonzalez0908@gmail.com>
2025-04-11 22:03:30 -0300

    Se agrego funcionalidad de agregar y quitar task

---
+++ index.js
@@ -0,0 +1,42 @@
+let listContainer = document.querySelector("#listContainer");
+let listInput = document.getElementById("listInput");
+
+// 1. Insertar la tarea en el DOM
+
+listInput.addEventListener("click", (e) => {
+  let element = e.target;
+  let textarea = listInput.querySelector("textarea");
+
+  if (element.type === "checkbox" && textarea.value !== "") {
+    let dataId = Date.now();
+
+    function AddTask() {
+      const taskHTML = `
+              <div data-id="${dataId}" class="bg-gray-800 rounded-t-full w-full flex items-center py-4 px-3 gap-3 border-b border-gray-600">
+                <input class="w-7 h-6 rounded-full appearance-none outline border-white border-1 checked:bg-[url(/images/icon-check.svg)] checked:m-auto cursor-pointer checked:bg-no-repeat checked:bg-[4px]" type="checkbox" name="check" id="check">
+                <label class="w-full text-white text-xs cursor-pointer align-middle" for="check">${textarea.value}</label>
+                <img class="w-4 h-4 cursor-pointer" src="images/icon-cross.svg" alt="cross">
+              </div>`;
+
+      return taskHTML;
+    }
+
+    listContainer.insertAdjacentHTML("afterbegin", AddTask());
+    textarea.value = "";
+  }
+});
+listContainer.addEventListener('click', (e) => {
+
+    let element = e.target;
+    // Verificamos si se hizo click en una cruz
+    if (element === element.closest('img')) {
+       
+        const taskId = element.parentNode.getAttribute('data-id');
+        const taskToRemove = listContainer.querySelector(`[data-id="${taskId}"]`);
+        if (taskToRemove) {
+            taskToRemove.remove();
+        }
+        
+       
+    }
+});
